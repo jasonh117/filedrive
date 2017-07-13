990e02fdba501be9abda6cc7a552ec7593a5a597
Jason Hsieh <jasonh117@hotmail.com>
2017-07-13 14:22:12 -0700

    setup sequelize

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const nconf = require('nconf');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const env = (process.env.NODE_ENV !== undefined) ? process.env.NODE_ENV.trim() : 'development';
+nconf.argv().env().file({ file: `${__dirname}/config.${env}.json` });
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,12 +23,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Set up routes
+const appRoutes = nconf.get('routes');
+appRoutes.forEach((item) => {
+  app.use(item.url, require(`${__dirname}/${item.file}`)); // eslint-disable-line
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- models/index.js
+++ models/index.js
@@ -1,36 +1,18 @@
-'use strict';
+const Sequelize = require('sequelize');
+const nconf = require('nconf');
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const postgresConfig = nconf.get('postgres');
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  postgresConfig.database,
+  postgresConfig.username,
+  postgresConfig.password,
+  postgresConfig.options
+);
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const db = {
+  sequelize,
+  Sequelize
+};
 
 module.exports = db;
