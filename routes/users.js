d8c1a209e890209fa33e8d1dbc93d8f2fdee42be
Jason Hsieh <jasonh117@hotmail.com>
2017-07-13 16:05:42 -0700

    add register route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const router = express.Router();
+const HTTPStatus = require('http-status');
+const models = require('../models');
+const { errorCodes } = require('../lib/error');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+const UserModel = models.User;
+
+router.post('/', (req, res) => {
+  if (!req.body.email || !req.body.email.trim()) {
+    res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_EMAIL });
+    return;
+  }
+
+  if (!req.body.password || !req.body.password.trim()) {
+    res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_PASSWORD });
+    return;
+  }
+
+  UserModel.create(req.body)
+    .then((user) => {
+      res.status(HTTPStatus.CREATED).json(user);
+    })
+    .catch((error) => {
+      if (error.name === 'SequelizeUniqueConstraintError') {
+        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_EMAIL_TAKEN });
+        return;
+      }
+      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+    });
 });
 
 module.exports = router;
