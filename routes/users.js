c851c45b7464b3a2efaa1f6d6c0de13155990905
Jason Hsieh <jasonh117@hotmail.com>
2017-07-14 11:28:43 -0700

    add user update route

--- routes/users.js
+++ routes/users.js
@@ -68,4 +68,28 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.patch('/', jwt.authenticate, (req, res) => {
+  const options = {
+    where: { id: req.user.id },
+    returning: true
+  };
+
+  UserModel.update(req.body, options)
+    .then((data) => {
+      if (!data || data[0] === 0) {
+        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_NOT_FOUND });
+        return;
+      }
+      const userObj = data[1][0].toJSON();
+      res.status(HTTPStatus.OK).json({ data: userObj });
+    })
+    .catch((error) => {
+      if (error.name === 'SequelizeUniqueConstraintError') {
+        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_EMAIL_TAKEN });
+        return;
+      }
+      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+    });
+});
+
 module.exports = router;
