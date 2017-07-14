1061b9863a30cef3e00d0c848e8af00d43789ba8
Jason Hsieh <jasonh117@hotmail.com>
2017-07-14 10:17:31 -0700

    add login route

--- routes/users.js
+++ routes/users.js
@@ -33,4 +33,39 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  if (!req.body.email || !req.body.email.trim()) {
+    res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_EMAIL });
+    return;
+  }
+
+  if (!req.body.password || !req.body.password.trim()) {
+    res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_PASSWORD });
+    return;
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  UserModel.findOne({ where: { email } })
+    .then((user) => {
+      if (!user) {
+        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_NOT_FOUND });
+        return;
+      }
+
+      if (!user.comparePassword(password)) {
+        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_INVALID_CREDENTIALS });
+        return;
+      }
+
+      const userObj = user.toJSON();
+      userObj.jwt = jwt.generateToken(userObj);
+      res.status(HTTPStatus.OK).json({ data: userObj });
+    })
+    .catch((error) => {
+      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+    });
+});
+
 module.exports = router;
