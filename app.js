6eea3343a41062260cc56bd0802aeaf4611e72ee
Jason Hsieh <jasonh117@hotmail.com>
2017-07-20 13:39:58 -0700

    add cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const nconf = require('nconf');
+const cors = require('cors');
 
 const env = (process.env.NODE_ENV !== undefined) ? process.env.NODE_ENV.trim() : 'development';
 nconf.argv().env().file({ file: `${__dirname}/config.${env}.json` });
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
