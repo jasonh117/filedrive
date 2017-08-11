64684478c031d58489ad42ab910d50f7e324dcd1
Jason Hsieh <jasonh117@hotmail.com>
2017-08-10 23:55:52 -0700

    update all routes to use async

--- routes/files.js
+++ routes/files.js
@@ -20,7 +20,7 @@ const orderbyTable = {
 
 router.use(jwt.authenticate);
 
-router.post('/', upload.any(), (req, res) => {
+router.post('/', upload.any(), async (req, res) => {
   if (!req.files || !req.files.length || req.files.length < 1) {
     res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_FILES });
     return;
@@ -43,37 +43,35 @@ router.post('/', upload.any(), (req, res) => {
     returning: true
   };
 
-  FileModel.bulkCreate(files, options)
-    .then((returnFiles) => {
-      res.status(HTTPStatus.CREATED).json({ data: returnFiles });
-    })
-    .catch((error) => {
-      if (error.name === 'SequelizeUniqueConstraintError') {
-        req.files.map(file => fs.unlinkSync(file.path));
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.FILE_SAME_NAME });
-        return;
-      }
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+  try {
+    const data = await FileModel.bulkCreate(files, options);
+    res.status(HTTPStatus.CREATED).json({ data });
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      req.files.map(file => fs.unlinkSync(file.path));
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.FILE_SAME_NAME });
+      return;
+    }
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const userId = req.user.id;
   const folder = req.query.folder || '/';
   const orderby = orderbyTable[req.query.sort] || 'lower("originalname")';
   const desc = req.query.desc === 'true' ? 'DESC' : 'ASC';
   const query = `SELECT * FROM files WHERE "userId"=${userId} AND folder='${folder}' ORDER BY ${orderby} ${desc};`;
 
-  sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
-    .then((returnFiles) => {
-      res.status(HTTPStatus.OK).json({ data: returnFiles });
-    })
-    .catch((error) => {
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+  try {
+    const data = await sequelize.query(query, { type: sequelize.QueryTypes.SELECT });
+    res.status(HTTPStatus.OK).json({ data });
+  } catch (error) {
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
-router.get('/:filename', (req, res) => {
+router.get('/:filename', async (req, res) => {
   const userId = req.user.id;
   const filename = req.params.filename;
   const view = req.query.view;
@@ -82,26 +80,27 @@ router.get('/:filename', (req, res) => {
     filename
   };
 
-  FileModel.findOne({ where })
-    .then((file) => {
-      if (!file) {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.FILE_INVALID_NAME });
-      }
-      if (view) {
-        return res.status(HTTPStatus.OK)
-          .sendFile(path.resolve(fileConfig.location, file.filename), {
-            headers: { 'Content-Type': file.mimetype }
-          });
-      }
-      return res.status(HTTPStatus.OK)
-        .download(path.resolve(fileConfig.location, file.filename), file.originalname);
-    })
-    .catch((error) => {
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+  try {
+    const file = await FileModel.findOne({ where });
+    if (!file) {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.FILE_INVALID_NAME });
+      return;
+    }
+    const filePath = path.resolve(fileConfig.location, file.filename);
+    if (view) {
+      const options = {
+        headers: { 'Content-Type': file.mimetype }
+      };
+      res.status(HTTPStatus.OK).sendFile(filePath, options);
+      return;
+    }
+    res.status(HTTPStatus.OK).download(filePath, file.originalname);
+  } catch (error) {
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
-router.delete('/:filename', (req, res) => {
+router.delete('/:filename', async (req, res) => {
   const userId = req.user.id;
   const filename = req.params.filename;
   const where = {
@@ -109,21 +108,19 @@ router.delete('/:filename', (req, res) => {
     filename
   };
 
-  FileModel.findOne({ where })
-    .then((file) => {
-      if (!file) {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.FILE_INVALID_NAME });
-      }
-      const filePath = path.resolve(fileConfig.location, file.filename);
-      if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
-      return file.destroy();
-    })
-    .then(() => {
-      res.status(HTTPStatus.OK).end();
-    })
-    .catch((error) => {
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+  try {
+    const file = await FileModel.findOne({ where });
+    if (!file) {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.FILE_INVALID_NAME });
+      return;
+    }
+    const filePath = path.resolve(fileConfig.location, file.filename);
+    if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+    await file.destroy();
+    res.status(HTTPStatus.OK).end();
+  } catch (error) {
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const jwt = require('../lib/jwt');
 const router = express.Router();
 const UserModel = models.User;
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   if (!req.body.email || !req.body.email.trim()) {
     res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_EMAIL });
     return;
@@ -18,22 +18,21 @@ router.post('/', (req, res) => {
     return;
   }
 
-  UserModel.create(req.body)
-    .then((user) => {
-      const userObj = user.toJSON();
-      userObj.jwt = jwt.generateToken(userObj);
-      res.status(HTTPStatus.CREATED).json({ data: userObj });
-    })
-    .catch((error) => {
-      if (error.name === 'SequelizeUniqueConstraintError') {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_EMAIL_TAKEN });
-        return;
-      }
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+  try {
+    const user = await UserModel.create(req.body);
+    const userObj = user.toJSON();
+    userObj.jwt = jwt.generateToken(userObj);
+    res.status(HTTPStatus.CREATED).json({ data: userObj });
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_EMAIL_TAKEN });
+      return;
+    }
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   if (!req.body.email || !req.body.email.trim()) {
     res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.MISSING_EMAIL });
     return;
@@ -47,49 +46,47 @@ router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  UserModel.findOne({ where: { email } })
-    .then((user) => {
-      if (!user) {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_NOT_FOUND });
-        return;
-      }
+  try {
+    const user = await UserModel.findOne({ where: { email } });
+    if (!user) {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_NOT_FOUND });
+      return;
+    }
 
-      if (!user.comparePassword(password)) {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_INVALID_CREDENTIALS });
-        return;
-      }
+    if (!user.comparePassword(password)) {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_INVALID_CREDENTIALS });
+      return;
+    }
 
-      const userObj = user.toJSON();
-      userObj.jwt = jwt.generateToken(userObj);
-      res.status(HTTPStatus.OK).json({ data: userObj });
-    })
-    .catch((error) => {
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+    const userObj = user.toJSON();
+    userObj.jwt = jwt.generateToken(userObj);
+    res.status(HTTPStatus.OK).json({ data: userObj });
+  } catch (error) {
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
-router.patch('/', jwt.authenticate, (req, res) => {
+router.patch('/', jwt.authenticate, async (req, res) => {
   const options = {
     where: { id: req.user.id },
     returning: true
   };
 
-  UserModel.update(req.body, options)
-    .then((data) => {
-      if (!data || data[0] === 0) {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_NOT_FOUND });
-        return;
-      }
-      const userObj = data[1][0].toJSON();
-      res.status(HTTPStatus.OK).json({ data: userObj });
-    })
-    .catch((error) => {
-      if (error.name === 'SequelizeUniqueConstraintError') {
-        res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_EMAIL_TAKEN });
-        return;
-      }
-      res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
-    });
+  try {
+    const data = await UserModel.update(req.body, options);
+    if (!data || data[0] === 0) {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_NOT_FOUND });
+      return;
+    }
+    const userObj = data[1][0].toJSON();
+    res.status(HTTPStatus.OK).json({ data: userObj });
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      res.status(HTTPStatus.BAD_REQUEST).json({ error: errorCodes.USER_EMAIL_TAKEN });
+      return;
+    }
+    res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
 module.exports = router;
